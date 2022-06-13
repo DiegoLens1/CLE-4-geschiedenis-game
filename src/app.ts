dda8017564968b1f7549bc45ca34e253f747e315
Veerle <1037333@hr.nl>
2022-06-13 10:02:19 +0200

    final app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js'
 import { Game } from './game';
-import bonesImage from "./images/start.jpg"
+import startImage from "./images/start.jpg"
 import rickyImage from "./images/Ricky.png"
 import paperImage from "./images/paperbg.jpg"
 
@@ -18,7 +18,7 @@ export class App{
         document.body.appendChild(this.pixi.view);
 
         this.loader = new PIXI.Loader();
-        this.loader.add('startTexture', bonesImage);
+        this.loader.add('startTexture', startImage);
         this.loader.add('rickyTexture', rickyImage);
         this.loader.add('paperTexture', paperImage);
         this.loader.load(()=>this.loadCompleted());
@@ -31,16 +31,16 @@ export class App{
         this.pixi.stage.addChild(paper);
     
 
-        let bones = new PIXI.Sprite(this.loader.resources["startTexture"].texture!);
-        bones.scale.set(0.3);
-        bones.anchor.set(0.5);
-        bones.x = 290;
-        bones.y = 200;
+        let start = new PIXI.Sprite(this.loader.resources["startTexture"].texture!);
+        start.scale.set(0.3);
+        start.anchor.set(0.5);
+        start.x = 290;
+        start.y = 200;
         
-        this.pixi.stage.addChild(bones);
+        this.pixi.stage.addChild(start);
 
-        bones.interactive = true; 
-        bones.buttonMode = true;
+        start.interactive = true; 
+        start.buttonMode = true;
 
         let ricky = new PIXI.Sprite(this.loader.resources["rickyTexture"].texture!);
 
@@ -51,7 +51,7 @@ export class App{
 
         this.pixi.stage.addChild(ricky);
 
-        bones.on('pointerdown', () => this.onClick());
+        start.on('pointerdown', () => this.onClick());
     }
 
     onClick(){

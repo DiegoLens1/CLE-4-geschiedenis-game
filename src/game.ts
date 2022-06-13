6addc94de476889a6bf353524d35d620a8ba7d50
Diego <d.lens2712@gmail.com>
2022-06-13 13:18:34 +0200

    changed inv

--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js'
 import fishImage from "./images/fish.png"
-import waterImage from "./images/water.jpg"
+import backgroundImage from "./images/background.png"
 import textboxImage from "./images/textbox.png"
 import rickyImage from "./images/Ricky.png"
 import boer1Image from "./images/boer1.png"
@@ -23,7 +23,7 @@ let width = 800
 export class Game {
     pixi: PIXI.Application
     textbox: Textbox
-    charachters: Character[] = []
+    characters: Character[] = []
     ricky:Ricky
     menu:Menu
     pot:Pot
@@ -35,7 +35,7 @@ export class Game {
 
         this.pixi.loader
             .add("fishTexture", boer2Image)
-            .add("waterTexture", waterImage)
+            .add("backgroundTexture", backgroundImage)
             .add("textboxTexture", textboxImage)
             .add("rickyTexture", rickyImage)
             .add("boer1Texture", boer1Image)
@@ -53,10 +53,23 @@ export class Game {
         let bgMusic = this.pixi.loader.resources["bgMusic"].data!
         bgMusic.play()
 
-        let water = new PIXI.Sprite(this.pixi.loader.resources["waterTexture"].texture!)
-        this.pixi.stage.addChild(water)
+        let background = new PIXI.Sprite(this.pixi.loader.resources["backgroundTexture"].texture!)
+        background.scale.set(1.32);
+        
+        this.pixi.stage.addChild(background)
             this.ricky = new Ricky(this.pixi.loader.resources["rickyTexture"].texture!)
+            this.ricky.scale.set(0.3);
+            this.ricky.y = 150;
+
+            let boer1Image = new Character(this.pixi.loader.resources["boer1Texture"].texture!)
+            boer1Image.scale.set(0.3);
+            boer1Image.x = 400;
+            boer1Image.y = 100;
+
+        
             this.pixi.stage.addChild(this.ricky)
+            this.pixi.stage.addChild(boer1Image)
+         
 
         this.textbox = new Textbox(this.pixi.loader.resources["textboxTexture"].texture!)
         this.pixi.stage.addChild(this.textbox)
@@ -66,42 +79,20 @@ export class Game {
         this.pixi.stage.addChild(this.menu)
         this.pot = new Pot(this.pixi.loader.resources["potTexture"].texture!)
         this.pixi.stage.addChild(this.pot)
+        this.pot.on('pointerdown', () => this.pot.onClick());
         this.speer = new Speer(this.pixi.loader.resources["speerTexture"].texture!)
         this.pixi.stage.addChild(this.speer)
+        this.speer.on('pointerdown', () => this.speer.onClick());
 
         this.pixi.ticker.add((delta)=>this.update(5));
     }
 
     update(delta:number){
-        this.ricky.walk()
+        // this.ricky.walk()
 
     }
 }
 
 
-export class Fish {
-    fish: PIXI.Sprite
-    startingY: number
 
-    constructor(pixi: PIXI.Application) {
-        this.fish = new PIXI.Sprite(pixi.loader.resources["fishTexture"].texture!)
-        this.fish.x = 500
-        this.startingY = 700
-        this.fish.anchor.set(0.5)
-        this.fish.scale.set(0.2)
-        pixi.stage.addChild(this.fish)
-        pixi.ticker.add((delta) => this.update(5))
-    }
-
-    update(delta: number) {
-        if (this.fish.x < -200) {
-            console.log('dag visje')
-            this.fish.x = width + 100
-        }
-        this.fish.x -= 0.1 * delta
-        this.fish.y += Math.sin(this.fish.x * 0.02) * 0.5
-        this.fish.angle = ((Math.sin(this.fish.x * 0.02) * 0.5-4)/(-4-4) * (360-0)) -180
-    }
-
-}
 

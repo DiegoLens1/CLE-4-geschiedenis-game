1983adb65f2b9e6a7adc90523fa52739f3164e55
Veerle <1037333@hr.nl>
2022-06-13 09:58:35 +0200

    app change

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,9 @@ export class App{
 
 
     constructor(){
-        this.pixi = new PIXI.Application({ width: this.pixiWidth, height: this.pixiHeight }); //vullen van pixi application 
-        this.pixi.stage.interactive = true; //om dingen te kunnen aanklikken
-        this.pixi.stage.hitArea = this.pixi.renderer.screen; //applicatie controleer op het hele scherm of er geklikt wordt
+        this.pixi = new PIXI.Application({ width: this.pixiWidth, height: this.pixiHeight }); 
+        this.pixi.stage.interactive = true; 
+        this.pixi.stage.hitArea = this.pixi.renderer.screen;
         document.body.appendChild(this.pixi.view);
 
         this.loader = new PIXI.Loader();

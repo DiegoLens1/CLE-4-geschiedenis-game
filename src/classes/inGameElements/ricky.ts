026fbb31b033144cfcb5aa3d09fcd7ff2cb2bc11
Ole van der Heiden <olejonasvanderheiden@gmail.com>
2022-06-13 13:41:39 +0200

    Time transition final version

--- src/classes/inGameElements/ricky.ts
+++ src/classes/inGameElements/ricky.ts
@@ -3,7 +3,25 @@ import * as PIXI from "pixi.js";
 
 export class Ricky extends Character
 {
-    constructor(sprite:PIXI.Texture) {
-        super(sprite);
+    private _timeTravel: boolean = false;
+
+    get timeTravel(): boolean {
+        return this._timeTravel;
     }
+
+    set timeTravel(timeTravel: boolean) {
+        this._timeTravel = timeTravel;
+    }
+
+    constructor(sprite:PIXI.Texture) {
+        super(sprite);2
+    }
+    timeTransition()
+    {
+        if (this.timeTravel == true) {
+            this.scale.x - 0.1
+            this.scale.y - 0.1
+        }
+    }
+
 }

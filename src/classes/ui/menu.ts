84e37c67ea59cd1ee77c3c292e8c312b6080c4bf
Ole van der Heiden <olejonasvanderheiden@gmail.com>
2022-06-20 11:21:54 +0200

    Updating the button class adding stopButton to menu and first version stopButton created

--- src/classes/ui/menu.ts
+++ src/classes/ui/menu.ts
@@ -2,12 +2,14 @@ import * as PIXI from 'pixi.js'
 import {Game} from '../../game'
 import {InventoryItem} from "../inGameElements/inventoryItem"
 import {Speer} from "../inGameElements/speer"
+import {StopButton} from "./stopButton";
 
 
 export class Menu extends PIXI.Sprite {
     pot: InventoryItem
     speer: InventoryItem
     pixi: PIXI.Application
+    stopButton: StopButton
 
     constructor(pixi: PIXI.Application, texture: PIXI.Texture, height: number, width: number) {
         super(texture)
@@ -26,12 +28,14 @@ export class Menu extends PIXI.Sprite {
     private createInventory() {
         this.pot = new InventoryItem(this.pixi.loader.resources["potTexture"].texture!, -450, -50, 1.4)
         this.speer = new InventoryItem(this.pixi.loader.resources["speerTexture"].texture!,-180,-60,0.55)
+        this.stopButton = new StopButton(this.pixi.loader.resources["stopButton"].texture!,420,-343,50,212)
         this.speer.angle = 10;
         this.pot.alpha = 0.2
         this.speer.alpha = 0.2
         // this.pixi.stage.addChild(this.pot)
         this.addChild(this.pot)
         this.addChild(this.speer)
+        this.addChild(this.stopButton)
     }
 
 
